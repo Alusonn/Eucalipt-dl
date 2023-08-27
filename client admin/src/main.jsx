9884960785cc33c33d33cbdfc77eacb36ca3681f
Alusonn <alanlossendiere@gmail.com>
2023-08-26 21:03:04 -0300

    Creacion de carpetas admin y public

---
+++ client admin/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EucaliptoApp } from "./EucaliptoApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EucaliptoApp />
+  </React.StrictMode>
+);
---
+++ client admin/src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+export const AppRouter = () => {
+
+    authStatus = 'authenticated'
+
+  return (
+    <Routes>
+        <Route path='/admin' element={} />
+    </Routes>
+  )
+}
