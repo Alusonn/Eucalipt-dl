19cccd9fa2c498683cec46aa2f9aeac586559bde
Alusonn <alanlossendiere@gmail.com>
2023-09-08 21:23:41 -0300

    ah re

---
+++ cliente public/src/store/cartUiSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const adminSlice = createSlice({
+  name: "admin",
+  initialState: {
+    products: [],
+    isOpen: false,
+  },
+  reducers: {
+
+  },
+});
+
+export const {  } = adminSlice.actions;
