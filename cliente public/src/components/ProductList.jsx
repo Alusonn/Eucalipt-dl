a39e186f99b9068b94da84daf2b0216ca78ac43e
Alan <alanlossendiere@gmail.com>
2023-09-12 00:09:40 -0300

    Listo estilos

--- cliente public/src/components/ProductList.jsx
+++ cliente public/src/components/ProductList.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export const ProductList = ({name = "Remera", price = 400,  }) => {
+export const ProductList = ({ name = "Remera", price = 400 }) => {
+
+  const onClickCart = () => {
+    console.log("Añadido al carrito de compras de Eucalipto-dl")
+  }
+
   return (
     <>
-    <div className="d-inline-block col-4 text-center px-2 py-2">
-      <div>{name}</div>
-      <div>{price}</div>
-    </div>
+      <div className="d-inline-block col-3 text-center px-2 py-2 card-body">
+        <div className="card-title">{name}</div>
+        <div className="card-text">$ {price}</div>
+        <div className="d-inline-block my-3 py-2 px-4 addCart" onClick={onClickCart}>
+          <div className="card-text">Añadir al carrito</div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
