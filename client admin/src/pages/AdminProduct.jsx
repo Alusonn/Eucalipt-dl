9d2e0b44e5a2aee27122f2b38146e7ff23ba8a19
Alusonn <alanlossendiere@gmail.com>
2023-09-13 23:23:37 -0300

    listo form

--- client admin/src/pages/AdminProduct.jsx
+++ client admin/src/pages/AdminProduct.jsx
@@ -29,44 +29,51 @@ export const AdminProduct = () => {
       <div className="row">
         <div className="col-4">
           <div className="form-floating mb-3">
-            <label for>Nombre del producto:</label>
-            <input type="text" value={name} className="form-control" />
+            <input
+              type="text"
+              value={name}
+              className="form-control"
+              id="floatingName"
+            />
+            <label for="floatingName">Nombre del producto:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" id="floatingDescription" placeholder="Descripcion..."/>
+            <label for="floatingDescription">Description:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="text" value={type} className="form-control" />
+            <input type="text" value={type} className="form-control" id="floatingType" placeholder="Tipo..."/>
+            <label for="floatingType">Tipo:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="number" value={price} className="form-control" />
+            <input type="number" value={price} className="form-control" id="floatingPrice" placeholder="Precio..."/>
+            <label for="floatingPrice">Precio:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="number" value={sku} className="form-control" />
+            <input type="number" value={sku} className="form-control" id="floatingSku" placeholder="sku"/>
+            <label for="floatingSku">SKU:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
             <input
               type="boolean"
               value={outstanding}
               className="form-control"
+              placeholder="Destacado..."
+              id="floatingOutstanding"
             />
+            <label for="floatingOutstanding">Destacado:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="boolean" value={sold} className="form-control" />
+            <input type="boolean" value={sold} className="form-control" id="floatingSold" placeholder="Vendido..."/>
+            <label for="floatingSold">Vendido:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="boolean" value={active} className="form-control" />
+            <input type="boolean" value={active} className="form-control" id="floatingActive" placeholder="Activo..."/>
+            <label for="floatingActive">Activo:</label>
           </div>
           <div className="form-floating mb-3">
-            <label for>:</label>
-            <input type="checkbox" value={sizes} className="form-control" />
+            <input type="checkbox" value={sizes} className="form-control" id="floatingSizes" placeholder="Talle..."/>
+            <label for="floatingSizes">Talle:</label>
           </div>
         </div>
         <div></div>
